b5b8dd541614cab643e0fb81e9ffb24aae84d5dc
RoyChen <roy850323@gmail.com>
2019-07-07 19:01:17 +0800

    sample2
    update

---
+++ sample2/js/index.js
@@ -0,0 +1,20 @@
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 1000 ||
+    document.documentElement.scrollTop > 1000
+  ) {
+    document.getElementById("go-top").style.display = "block";
+  } else {
+    document.getElementById("go-top").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
