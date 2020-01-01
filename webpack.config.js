6b0729794a4f4ae288e270bf1d49c37d0775e133
RoyChen <roy850323@gmail.com>
2020-01-02 01:01:36 +0800

    update
    npm init
    npm i gh-pages

--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-module.exports = {
-    entry: './js/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, './'),
-    }
-};
